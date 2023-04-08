143ec3909a718b4453f3ba4e4bfc827ddf5acc64
DESKTOP-4BHM0EO\ivandrian11 <ivandrian2024@gmail.com>
2023-04-08 20:18:29 +0800

    Revert "update semantic"
    This reverts commit 6013add861a646d34440bb6b8a495de9f587c221.

--- src/js/components/trend-movie.js
+++ src/js/components/trend-movie.js
@@ -14,9 +14,9 @@ class TrendMovie extends HTMLElement {
         }"
             alt="${this._item.title}" class="object-fill h-64 rounded-lg w-72">
         <header class="absolute bottom-0 w-[264px] m-3 glass-effect p-3 rounded-lg">
-            <h3 class="font-semibold whitespace-nowrap overflow-clip">
+            <h4 class="font-semibold whitespace-nowrap overflow-clip">
                 ${this._item.title}
-            </h3>
+            </h4>
             <p class="text-xs text-gray-2 whitespace-nowrap overflow-clip">
             ${this._item.release_date.slice(0, 4)} • ${this._item.genres.join(
       ', '
